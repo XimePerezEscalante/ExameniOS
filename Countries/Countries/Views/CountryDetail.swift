07e5f0d4a270794af805c680f40cb819c8bdea8f
XimePerezEscalante <A01751827@tec.mx>
2025-10-14 17:26:37 -0600

    feat: added detail view

--- Countries/Countries/Views/CountryDetail.swift
+++ Countries/Countries/Views/CountryDetail.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct CountryDetail: View {
+    var country: Country
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 16) {
+                Text(country.name.common!)
+                    .font(.title)
+                HStack(spacing: 10) {
+                    Text("Official name:")
+                        .fontWeight(.medium)
+                    Text(country.name.official!)
+                }
+                HStack(spacing: 10) {
+                    Text("Subregion:")
+                        .fontWeight(.medium)
+                    Text(country.subregion!)
+                }
+                /*HStack(alignment: .leading, spacing: 10) {
+                    Text("Capital:")
+                        .fontWeight(.medium)
+                    Text("\(country.name.official)")
+                }
+                HStack(alignment: .leading, spacing: 10) {
+                    Text(":")
+                        .fontWeight(.medium)
+                    Text("\(country.name.official)")
+                }*/
+            }.padding(20)
+        }
+        
     }
-}
-
-#Preview {
-    CountryDetail()
 }
