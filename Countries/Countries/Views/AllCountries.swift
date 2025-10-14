1ed6ff9c643ed71118ad4c267a5f9ec7e566485f
XimePerezEscalante <A01751827@tec.mx>
2025-10-14 16:41:19 -0600

    feat: you can now see countries filtered by the continent they belong to

--- Countries/Countries/Views/AllCountries.swift
+++ Countries/Countries/Views/AllCountries.swift
@@ -25,8 +25,65 @@ struct AllCountries: View {
                     Text("Loading all countries...")
                         .font(.subheadline)
                 }
+                // Seccion de Africa
+                if !vm.africanCountries.isEmpty {
+                    Text("Africa")
+                        .font(.headline)
+                    List(vm.africanCountries) { country in
+                        NavigationLink(destination: CountryDetail(country: country)) {
+                            Text(country.name.common!)
+                        }
+                    }
+                    
+                }
+                // Seccion de America
+                if !vm.americanCountries.isEmpty {
+                    Text("America")
+                        .font(.headline)
+                    List(vm.americanCountries) { country in
+                        NavigationLink(destination: CountryDetail(country: country)) {
+                            Text(country.name.common!)
+                        }
+                    }
+                    
+                }
+                // Seccion de Asia
+                if !vm.asianCountries.isEmpty {
+                    Text("Asia")
+                        .font(.headline)
+                    List(vm.asianCountries) { country in
+                        NavigationLink(destination: CountryDetail(country: country)) {
+                            Text(country.name.common!)
+                        }
+                    }
+                    
+                }
+                // Seccion de Europa
+                if !vm.europeanCountries.isEmpty {
+                    Text("Europa")
+                        .font(.headline)
+                    List(vm.europeanCountries) { country in
+                        NavigationLink(destination: CountryDetail(country: country)) {
+                            Text(country.name.common!)
+                        }
+                    }
+                    
+                }
+                // Seccion de Oceania
+                if !vm.oceaniaCountries.isEmpty {
+                    Text("Oceania")
+                        .font(.headline)
+                    List(vm.oceaniaCountries) { country in
+                        NavigationLink(destination: CountryDetail(country: country)) {
+                            Text(country.name.common!)
+                        }
+                    }
+                    
+                }
+                
             }
-        }.navigationTitle("Countries")
+        }.padding(20)
+            .navigationTitle("Countries")
             .onAppear {
                 Task {
                     await vm.getAllCountries()
