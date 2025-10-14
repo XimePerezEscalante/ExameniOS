1ad569753c231c6b1881f0c0cc39c58a340d7861
XimePerezEscalante <A01751827@tec.mx>
2025-10-14 12:12:03 -0600

    doc: added Alamofire library

---
+++ Countries/Countries/Data/Models/Network/APIService.swift
@@ -0,0 +1,28 @@
+//
+//  APIService.swift
+//  Countries
+//
+//  Created by Ximena Perez Escalante on 14/10/25.
+//
+
+import Foundation
+import Alamofire
+
+struct APIService {
+    // Singleton
+    static let shared = APIService()
+    
+    func getAllCountries(url: URL) async {
+        
+        let taskRequest = AF.request(url, method: .get, encoding: JSONEncoding.default).validate()
+        let response = await taskRequest.serializingData().response
+        
+        switch response.result {
+        case .success(let data):
+            print("Successfully got all countries")
+            
+        case .failure(let error):
+            print("Failure getting countries:", error.localizedDescription)
+        }
+    }
+}
